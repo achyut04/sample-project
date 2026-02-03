8399dd71b52e575402ba9897239c0ac7efc47b18
achyut04 <achyutsavaliya4@gmail.com>
2026-02-03 23:06:35 +0530

    Footer and styling changes

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,9 +7,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="p-2">{children}</main>
+      <main className="p-2 flex-grow">{children}</main>
       <Footer />
     </div>
   );
